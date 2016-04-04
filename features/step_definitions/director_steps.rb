f68e1a3932989f2b99c7c99be3c3fe0dc564871d
steevymathew <steevy.mathew@gmail.com>
2016-04-04 18:30:38 +0000

    adding director

--- features/step_definitions/director_steps.rb
+++ features/step_definitions/director_steps.rb
@@ -4,11 +4,15 @@ Given /the following movies exist:/ do |table|
     end
 end
 
-When /^(?:|I )go to "(.+)" for "(.+)"$/ do |page_name,movie_name| 
+When /^(?:|I )go to the edit page for "(.+)"$/ do |movie_name| 
     visit('/')
     expect(page).to have_content("Include") #check to see if at home
-    click_link("More about"+movie_name)
+    click_link("More about "+movie_name)
     expect(page).to have_content("Details about "+movie_name) #check to see if at correct page
     click_link("Edit")
     expect(page).to have_content("Edit Existing Movie") #check to see if at correct page
+end
+
+And /^(?:|I )fill in "(.+)" with "(.+)"$/ do |form_field,form_entry| 
+    fill_in form_field, with: form_entry
 end
